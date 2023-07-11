e185ce4cdadfc2ffe347e526026b50a28036d26a
Galstyan Arutyun <reged1t.galar@icloud.com>
2023-07-11 20:37:25 +0300

    v2.0 deploy main.py upgrade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from pytz import timezone
+import datetime
 import httpx
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ParseMode
@@ -7,18 +6,15 @@ from aiogram.utils import executor
 from config import *
 from math import ceil
 
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-# Функция для преобразования UTC времени в локальное время пользователя
-def convert_utc_to_local(utc_time, tz):
-    utc = datetime.strptime(utc_time, "%A %d-%b-%Y %H:%M:%S")
-    local = utc.replace(tzinfo=timezone('UTC')).astimezone(timezone(tz))
-    return local.strftime('%H:%M')
 
 @dp.message_handler(commands=['start'])
 async def start_command(msg: types.Message):
     await msg.reply('Привет! Напиши мне название города, и я пришлю сводку погоды')
+
 
 @dp.message_handler()
 async def get_weather(msg: types.Message):
@@ -42,16 +38,9 @@ async def get_weather(msg: types.Message):
             pressure = main['pressure']
             wind_speed = wind['speed']
 
-            # Получаем текущую временную метку в UTC
-            utc_time = datetime.utcnow().strftime('%A %d-%b-%Y %H:%M:%S')
-
-            # Преобразуем временные метки в локальное время пользователя
-            tz = msg.from_user.tzinfo  # Получаем часовой пояс пользователя
-            sunrise_time = convert_utc_to_local(sys['sunrise'], tz)
-            sunset_time = convert_utc_to_local(sys['sunset'], tz)
-
-            # Вычисляем продолжительность дня
-            length_day = datetime.strptime(sys['sunset'], "%%A %d-%b-%Y %H:%M:%S") - datetime.strptime(sys['sunrise'], "%A %d-%b-%Y %H:%M:%S")
+            sunrise_timestamp = datetime.datetime.fromtimestamp(sys['sunrise'])
+            sunset_timestamp = datetime.datetime.fromtimestamp(sys['sunset'])
+            length_day = sunset_timestamp - sunrise_timestamp
 
             code_to_smile = {
                 "Clear": "Ясно ☀️",
@@ -65,19 +54,20 @@ async def get_weather(msg: types.Message):
             weather_description = code_to_smile.get(weather['main'], 'Посмотри в окно, я не понимаю, что там за погода...')
 
             await msg.reply(
-                f"{datetime.now(tz).strftime('%A %d-%b-%Y %H:%M')}\n"  # Выводим текущую дату и время в локальное время пользователя
+                f"{datetime.datetime.now().strftime('%A %d-%b-%Y %H:%M')}\n"
                 f"Погода в городе: {city}\nТемпература: {cur_weather} °C - {weather_description}\n"
                 f"Влажность: {humidity}%\nДавление: {ceil(pressure / 1.333)} мм.рт.ст\nВетер: {wind_speed} м/с \n"
-                f"Восход солнца: {sunrise_time}\nЗакат солнца: {sunset_time}\nПродолжительность дня: {length_day}\n"
+                f"Восход солнца: {sunrise_timestamp.strftime('%H:%M')}\nЗакат солнца: {sunset_timestamp.strftime('%H:%M')}\nПродолжительность дня: {length_day}\n"
                 f"Хорошего дня!",
                 parse_mode=ParseMode.HTML
             )
     except httpx.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
+        print(f"HTTP ошибка: {http_err}")
         await msg.reply('Ошибка при выполнении HTTP-запроса')
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Ошибка: {e}")
         await msg.reply('Произошла ошибка. Пожалуйста, попробуйте еще раз')
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
