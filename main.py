172f25617b58083e49e2089b7c4d1b5e92fddbe4
Galstyan Arutyun <reged1t.galar@icloud.com>
2023-07-11 22:18:24 +0300

    v3.0 deploy add local time sunset/sunrise output update requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-import datetime
+from datetime import datetime
+
+from pytz import timezone
 import httpx
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ParseMode
 from aiogram.utils import executor
 from config import *
 from math import ceil
+from timezonefinder import TimezoneFinder
 
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
+tf = TimezoneFinder()
+
+
+async def get_city_timezone(lat, lon):
+    tz_name = tf.timezone_at(lat=lat, lng=lon)
+    if tz_name:
+        return timezone(tz_name)
+
+    return None
 
 
 @dp.message_handler(commands=['start'])
@@ -31,6 +44,7 @@ async def get_weather(msg: types.Message):
             main = data['main']
             wind = data['wind']
             sys = data['sys']
+            coord = data['coord']
 
             city = data['name']
             cur_weather = main['temp']
@@ -38,34 +52,50 @@ async def get_weather(msg: types.Message):
             pressure = main['pressure']
             wind_speed = wind['speed']
 
-            sunrise_timestamp = datetime.datetime.fromtimestamp(sys['sunrise'])
-            sunset_timestamp = datetime.datetime.fromtimestamp(sys['sunset'])
-            length_day = sunset_timestamp - sunrise_timestamp
+            sunrise_timestamp = sys['sunrise']
+            sunset_timestamp = sys['sunset']
 
-            code_to_smile = {
-                "Clear": "Ясно ☀️",
-                "Clouds": "Облачно ☁️",
-                "Rain": "Дождь 🌧️",
-                "Drizzle": "Дождь 🌧️",
-                "Thunderstorm": "Гроза ⛈️",
-                "Snow": "Снег 🌨️",
-                "Mist": "Туман 🌫️"
-            }
-            weather_description = code_to_smile.get(weather['main'], 'Посмотри в окно, я не понимаю, что там за погода...')
+            # Получаем информацию о временной зоне города
+            city_timezone = await get_city_timezone(coord['lat'], coord['lon'])
 
-            await msg.reply(
-                f"{datetime.datetime.now().strftime('%A %d-%b-%Y %H:%M')}\n"
-                f"Погода в городе: {city}\nТемпература: {cur_weather} °C - {weather_description}\n"
-                f"Влажность: {humidity}%\nДавление: {ceil(pressure / 1.333)} мм.рт.ст\nВетер: {wind_speed} м/с \n"
-                f"Восход солнца: {sunrise_timestamp.strftime('%H:%M')}\nЗакат солнца: {sunset_timestamp.strftime('%H:%M')}\nПродолжительность дня: {length_day}\n"
-                f"Хорошего дня!",
-                parse_mode=ParseMode.HTML
-            )
+            if city_timezone:
+                # Преобразуем временные метки в локальное время города
+                sunrise_time = datetime.fromtimestamp(sunrise_timestamp, timezone('UTC')).astimezone(city_timezone).strftime('%H:%M')
+                sunset_time = datetime.fromtimestamp(sunset_timestamp, timezone('UTC')).astimezone(city_timezone).strftime('%H:%M')
+
+                # Вычисляем продолжительность дня в часах и минутах
+                length_day_seconds = sunset_timestamp - sunrise_timestamp
+                length_day_hours = length_day_seconds // 3600
+                length_day_minutes = (length_day_seconds % 3600) // 60
+
+                code_to_smile = {
+                    "Clear": "Ясно ☀️",
+                    "Clouds": "Облачно ☁️",
+                    "Rain": "Дождь 🌧️",
+                    "Drizzle": "Дождь 🌧️",
+                    "Thunderstorm": "Гроза ⛈️",
+                    "Snow": "Снег 🌨️",
+                    "Mist": "Туман 🌫️"
+                }
+                weather_description = code_to_smile.get(weather['main'],
+                                                        'Посмотри в окно, я не понимаю, что там за погода...')
+
+                await msg.reply(
+                    f"{datetime.now(city_timezone).strftime('%A %d-%m-%Y %H:%M')}\n"
+                    f"Погода в городе: {city}\nТемпература: {cur_weather} °C - {weather_description}\n"
+                    f"Влажность: {humidity}%\nДавление: {ceil(pressure / 1.333)} мм.рт.ст\nВетер: {wind_speed} м/с \n"
+                    f"Восход солнца: {sunrise_time}\nЗакат солнца: {sunset_time}\n"
+                    f"Продолжительность дня: {length_day_hours} часов {length_day_minutes} минут\n"
+                    f"Хорошего дня! 👋",
+                    parse_mode=ParseMode.HTML
+                )
+            else:
+                await msg.reply('Не удалось получить информацию о временной зоне для данного города.')
     except httpx.HTTPError as http_err:
-        print(f"HTTP ошибка: {http_err}")
+        print(f"HTTP error occurred: {http_err}")
         await msg.reply('Ошибка при выполнении HTTP-запроса')
     except Exception as e:
-        print(f"Ошибка: {e}")
+        print(f"An error occurred: {e}")
         await msg.reply('Произошла ошибка. Пожалуйста, попробуйте еще раз')
 
 
